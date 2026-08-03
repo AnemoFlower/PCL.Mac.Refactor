fb957c4e2237cfdd6fca78e505566de2c185e80a
wunanc <wunanc@163.com>
2026-08-03 15:52:00 +0800

    chore: 在所有任务完成之后自动回到主页面 (#224)

--- PCL.Mac/Managers/TaskManager.swift
+++ PCL.Mac/Managers/TaskManager.swift
@@ -9,6 +9,7 @@ import Foundation
 import Combine
 import Core
 
+@MainActor
 public class TaskManager: ObservableObject {
     public static let shared: TaskManager = .init()
     
@@ -23,7 +24,6 @@ public class TaskManager: ObservableObject {
     ///   - display: 是否显示与弹出 hint。
     ///   - completion: 任务完成回调。
     /// - Returns: 执行此任务的 Swift `Task`。
-    @MainActor
     @discardableResult
     public func execute<Model>(task: MyTask<Model>, display: Bool = true, completion: ((Error?) -> Void)? = nil) -> Task<Void, Error> {
         let id: UUID = task.id
@@ -67,9 +67,14 @@ public class TaskManager: ObservableObject {
         }
     }
     
+    @MainActor
     private func clean(for id: UUID) {
         tasks.removeAll(where: { $0.id == id })
         executorTasks.removeValue(forKey: id)
+        
+        if tasks.isEmpty, AppRouter.shared.last == .tasks {
+            AppRouter.shared.removeLast()
+        }
     }
     
     private init() {
